e662ad332707fad3d597973a4aa565e59b950aca
Albert Polo Molina <apolomo@uoc.edu>
2025-09-04 11:39:18 +0200

    Add files via upload

---
+++ update_radiation.py
@@ -0,0 +1,33 @@
+import requests
+import pandas as pd
+from io import StringIO
+import os
+
+url = 'https://opendata.aemet.es/opendata/api/red/especial/radiacion'
+headers = {
+    'accept': 'application/json',
+    'api_key': os.getenv("AEMET_API_KEY")
+}
+
+response_rad = requests.get(url, headers=headers)
+if response_rad.status_code == 200:
+    response_json = response_rad.json()
+    if 'datos' in response_json:
+        datos_url_rad = response_json['datos']
+        datos_texto = requests.get(datos_url_rad).text
+        data_to_text = '\n'.join(datos_texto.split('\n')[2:])
+        df = pd.read_csv(StringIO(data_to_text), sep=';')
+        df_valencia = df[df['Estación'] == 'Valencia Aeropuerto'].copy()
+        df_valencia.loc[:, 'Fecha'] = datos_texto.split('\r\n')[1]
+
+        archivo_csv = 'datos_radiacion.csv'
+
+        # Cargar CSV existente o crear uno nuevo
+        if os.path.exists(archivo_csv):
+            df_existente = pd.read_csv(archivo_csv)
+            df_combinado = pd.concat([df_existente, df_valencia])
+            df_combinado = df_combinado.drop_duplicates()
+        else:
+            df_combinado = df_valencia
+
+        df_combinado.to_csv(archivo_csv, index=False, encoding='utf-8')
